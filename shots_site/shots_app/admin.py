09404ac3ab3365c0e5ce55b6b00c66200ba5a881
unknown <jonathansamayoa1998@gmail.com>
2018-09-15 16:10:47 -0400

    Used Template in Views

--- shots_site/shots_app/admin.py
+++ shots_site/shots_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import DrinkingGame
-from .models import Trigger
-from .models import Action
 
+from .models import DrinkingGame
 admin.site.register(DrinkingGame)
+
+from .models import Trigger
 admin.site.register(Trigger)
+
+from .models import Action
 admin.site.register(Action)
--- shots_site/shots_app/views.py
+++ shots_site/shots_app/views.py
@@ -7,17 +7,14 @@ template = loader.get_template('shots_app/index.html')
 
 def home(request):
     latest_games = DrinkingGame.objects.order_by('-pub_date')[:5]
-    latest = 'Games Recently Created:\n' + ', '.join([g.title for g in latest_games])
 
     most_played = DrinkingGame.objects.order_by('-play_count')[:5]
-    popular = '\nMost Popular Games:\n' + ', '.join([g.title for g in most_played])
 
     top_rated = DrinkingGame.objects.order_by('-rating')[:5]
-    rat = '\nTop Rated:\n' + ', '.join([g.title for g in top_rated])
 
-    context = { 'latest_games' : latest,
-                'most_played' : popular,
-                'top_rated' : rat }
+    context = { 'latest_games' : latest_games,
+                'most_played' : most_played,
+                'top_rated' : top_rated }
 
     return HttpResponse(template.render(context, request))
 
