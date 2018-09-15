32a0e3ddb01243f7ba26023a52d5a5b94a22e1f6
Tanya Smith <tanika@mit.edu>
2018-09-15 14:31:39 -0400

    max lengths

--- shots_site/shots_app/models.py
+++ shots_site/shots_app/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class DrinkingGame(models.Model):
-    title = models.CharField() # max length?
-    #author = models.CharField(max_length = 0)
+    title = models.CharField(max_length = 200)
+    #author = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date created')
     play_date = models.DateTimeField('date last played')
     play_count = models.IntegerField(default = 0)
@@ -10,10 +10,10 @@ class DrinkingGame(models.Model):
 class Trigger(models.Model):
     drinking_game = models.ForeignKey(DrinkingGame, on_delete = models.CASCADE)
 
-    trigger = models.CharField() # max length?
+    trigger = models.CharField(max_length = 200)
 
 class Action(models.Model):
     trigger = models.ForeignKey(Trigger, on_delete = models.CASCADE)
 
     amount = models.IntegerField(default = 1)
-    description = models.CharField() # max length ?
+    description = models.CharField(max_length = 200)
