57f92dde3bf6db7bd3533cd284f994d27c8b5cdf
Tanya Smith <tanika@mit.edu>
2018-09-15 15:44:22 -0400

    anime titties

--- shots_site/shots_app/models.py
+++ shots_site/shots_app/models.py
@@ -6,6 +6,12 @@ class DrinkingGame(models.Model):
     pub_date = models.DateTimeField('date created')
     play_date = models.DateTimeField('date last played')
     play_count = models.IntegerField(default = 0)
+    rating = models.IntegerField(default = 0,
+                                 choices = [(1, 'A anime tittie'),
+                                            (2, 'B anime titties'),
+                                            (3, 'C anime titties'),
+                                            (4, 'D anime titties'),
+                                            (5, 'DD anime titties')])
 
     def __str__(self):
         return self.title
@@ -14,7 +20,7 @@ class Trigger(models.Model):
     drinking_game = models.ForeignKey(DrinkingGame, on_delete = models.CASCADE)
 
     description = models.CharField(max_length = 200)
-    
+
     def __str__(self):
         return self.description
 
--- shots_site/shots_app/views.py
+++ shots_site/shots_app/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Home Page")
+    latest_games = DrinkingGame.objects.order_by('-pub_date')[:5]
+    latest = 'Games Recently Created:\n' + ', '.join([g.title for g in latest_games])
+
+    most_played = DrinkingGame.objects.order_by('-play_count')[:5]
+    popular = '\nMost Popular Games:\n' + ', '.join([g.title for g in most_played])
+
+    top_rated = DrinkingGame.objects.order_by('-rating')[:5]
+    rat = '\nTop Rated:\n' + ', '.join([g.title for g in top_rated])
+
+    return HttpResponse(latest, popular, rat)
 
 def create(request):
     return HttpResponse("Create Page")
