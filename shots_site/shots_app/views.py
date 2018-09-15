c3d95c8adf0fd5b52c0c735785d4ff3ea4df5fdc
unknown <jonathansamayoa1998@gmail.com>
2018-09-15 13:27:06 -0400

    views

--- shots_site/shots_app/views.py
+++ shots_site/shots_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Home Page")
+
+def create(request):
+    return HttpResponse("Create Page")
+
+def play(request):
+    return HttpResponse("Play Page")
