e9cec46fcf5987be307f5d284fd786fb7608c85d
unknown <jonathansamayoa1998@gmail.com>
2018-09-15 15:04:28 -0400

    Admin stuff

--- shots_site/shots_app/admin.py
+++ shots_site/shots_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import DrinkingGame
+admin.site.register(DrinkingGame)
+
+from .models import Trigger
+admin.site.register(Trigger)
+
+from .models import Action
+admin.site.register(Action)
--- shots_site/shots_app/models.py
+++ shots_site/shots_app/models.py
@@ -7,13 +7,21 @@ class DrinkingGame(models.Model):
     play_date = models.DateTimeField('date last played')
     play_count = models.IntegerField(default = 0)
 
+    def __str__(self):
+        return self.title
+
 class Trigger(models.Model):
     drinking_game = models.ForeignKey(DrinkingGame, on_delete = models.CASCADE)
 
-    trigger = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 200)
+    def __str__(self):
+        return self.description
 
 class Action(models.Model):
     trigger = models.ForeignKey(Trigger, on_delete = models.CASCADE)
 
     amount = models.IntegerField(default = 1)
     description = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.description + str(amount) + "times"
