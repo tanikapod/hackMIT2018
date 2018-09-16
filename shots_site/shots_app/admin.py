666f121141621a9b7db8b41521fb42c6e33412dc
unknown <jonathansamayoa1998@gmail.com>
2018-09-15 23:20:49 -0400

    Test5400

--- shots_site/shots_app/admin.py
+++ shots_site/shots_app/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
+from .models import *
 
-from .models import DrinkingGame
 admin.site.register(DrinkingGame)
-
-from .models import Trigger
 admin.site.register(Trigger)
-
-from .models import Action
-admin.site.register(Action)
