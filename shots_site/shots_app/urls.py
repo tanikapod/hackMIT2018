36b5654c68589dfb4425fb3fc840c9c8eb5254fa
unknown <jonathansamayoa1998@gmail.com>
2018-09-15 23:18:17 -0400

    Testing scripts more

--- shots_site/shots_app/urls.py
+++ shots_site/shots_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('create/', views.create, name = 'create'),
     path('<int:game_id>/', views.game_detail, name = 'game'),
-    path('play/', views.play, name = 'play')
+    path('play/', views.play, name = 'play'),
+    url(r'^create', views.make_drinking_game)
 ]
--- shots_site/shots_site/urls.py
+++ shots_site/shots_site/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('shots/', include('shots_app.urls')),
     path('admin/', admin.site.urls),
+
 ]
